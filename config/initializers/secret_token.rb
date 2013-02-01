4c5089ccdc75f61a21558c653a8aebd04cd50701
mikaelsn <mikael.snellman@cs.helsinki.fi>
2013-02-01 14:32:13 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Xrciser::Application.config.secret_token = '9003f98659c4000428f2ab4552db293e975fa192348fabfc98b33a445c13fed5f94f1eaad02bebe47c996690184fc8d0bb76132e0d1e5c35e67f6f16f7212763'
