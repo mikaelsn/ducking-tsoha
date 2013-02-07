a89f6ff2052ee729d776161606514cdad93d1e07
mikaelsn <mikael.snellman@cs.helsinki.fi>
2013-02-07 07:34:13 +0200

    validations

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,5 +1,6 @@
 class Activity < ActiveRecord::Base
   attr_accessible :description, :name
 
+  validates_presence_of :name
   belongs_to :exercise
 end
--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,5 +1,6 @@
 class Exercise < ActiveRecord::Base
   attr_accessible :feel, :time
 
+  validates_presence_of :feel
   has_many :activities
 end
