c8921af3a4ba6ea3bc4f62bf23283a0dbce35ae6
mikaelsn <mikael.snellman@cs.helsinki.fi>
2013-02-19 22:37:03 +0200

    validations and forms for activities

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,6 +1,8 @@
 class Activity < ActiveRecord::Base
   attr_accessible :description, :name, :weight, :sets, :reps
-
   validates_presence_of :name
+  validates :weight, :numericality => true
+  validates :sets, :numericality => { :only_integer => true }
+  validates :reps, :numericality => { :only_integer => true }
   belongs_to :exercise
 end
